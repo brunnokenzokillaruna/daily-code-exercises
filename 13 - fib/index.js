252fc2b7cfbd8abde528674226cfb425d37ea2ee
Brunno Kenzo <brunnokenzokillaruna@gmail.com>
2024-09-18 21:18:57 -0400

    Task: 13 - fib done

---
+++ 13 - fib/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function fib(n) {
+
+    let prev = 0;
+    let curr = 1;
+
+    if (n === 0) {
+        return 0;
+    }
+
+    for (let i = 2; i <= n; i++) {
+        let sum = prev + curr;
+        prev = curr;
+        curr = sum;
+    }
+
+    return curr;
+
+}
+
+module.exports = fib;
+
