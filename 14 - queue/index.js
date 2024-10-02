a0c71b9f31b3afc2ff3e667eb42ecdead7f11bf3
Brunno Kenzo <brunnokenzokillaruna@gmail.com>
2024-10-01 21:13:39 -0400

    Task: 14 - queue done

--- 14 - queue/index.js
+++ 14 - queue/index.js
@@ -8,6 +8,20 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.data = []; // Initialize an empty array to hold the elements of the queue.
+    }
+
+    add(value) {
+        // Add the value to the start of the array (beginning of the queue).
+        this.data.unshift(value);
+    }
+
+    remove() {
+        // Remove and return the value from the end of the array (end of the queue).
+        return this.data.pop();
+    }
+}
 
 module.exports = Queue;
